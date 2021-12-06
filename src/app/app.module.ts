ae8afd73fd50970a8a21b650249b01a4426c883e
Julio Garcia <julioferfreestyle@gmail.com>
2021-12-05 20:25:28 -0500

    Frontend (login y registro)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegistroComponent } from './registro/registro.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+ReactiveFormsModule;
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
